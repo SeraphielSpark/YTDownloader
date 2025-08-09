f1363392fe764061b0c84ce6e7e581770a464573
SeraphielSpark <taiwoemmanuel435@gmail.com>
2025-08-09 13:22:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -85,8 +85,7 @@ app.get('/download', (req, res) => {
     });
   });
 });
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+  console.log(`🚀 Server running on port ${PORT}`);
 });
